a56e4686761437976b16dcc9afaee8bd09ffa649
lalagaming1 <123898156+lalagaming1@users.noreply.github.com>
2023-02-01 18:02:29 +0200

    finished calculator

--- main.py
+++ main.py
@@ -20,6 +20,29 @@ while True:
     print("A:Addition")
     print("B:Substraction")
     print("C:Multiplication")
-    print("D:DIvision")
+    print("D:Division")
     print("E:Exit")
-    choice = input()
+    choice = input().lower()
+    #If Statements
+    if choice == "a":
+        first_number = input("What is the first number")
+        second_number = input("What is the second number")
+        add(first_number, second_number)
+    elif choice == "b":
+        first_number = input("What is the first number")
+        second_number = input("What is the second number")
+        sub(first_number, second_number)
+
+    elif choice == "c":
+        first_number = input("What is the first number")
+        second_number = input("What is the second number")
+        mul(first_number, second_number)
+
+    elif choice == "d":
+        first_number = input("What is the first number")
+        second_number = input("What is the second number")
+        div(first_number, second_number)
+
+    elif choice == "e":
+        break
+print("Bye!") 
